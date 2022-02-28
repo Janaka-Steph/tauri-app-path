27ad1e8e10aaa6764a332cd00cb400e837ed835e
Janaka-Steph <rochestephane@yahoo.fr>
2022-02-28 10:38:36 +0000

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { path } from '@tauri-apps/api';
+import tauriCircles from './tauri.svg'
+import tauriWord from './wordmark.svg'
+import './App.css'
+
+async function getAppDirPath(): Promise<string> {
+  return path.appDir();
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="inline-logo">
+          <img src={tauriCircles} className="App-logo rotate" alt="logo" />
+          <img src={tauriWord} className="App-logo smaller" alt="logo" />
+        </div>
+        <h1>App Directory</h1>
+        <p>{getAppDirPath()}</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
